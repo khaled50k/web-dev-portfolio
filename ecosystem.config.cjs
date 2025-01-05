0c488518a6e77af37e2a2fced8c54fa8862f2af3
ibdaatec <ibdaatec@gmail.com>
2025-01-05 10:32:25 +0200

    Update ecosystem configuration for 'author-ibdaatec' application, adding environment variables and clarifying script path

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,15 @@
 module.exports = {
     apps: [
       {
-        name: 'ibdaatec',
-        port: '3200',
-        exec_mode: 'cluster',
-        instances: 'max',
-        script: './.output/server/index.mjs'
-      }
-    ]
-  }
+        name: "author-ibdaatec", // Application name
+        script: ".output/server/index.mjs", // Path to the built Nuxt.js server entry point
+        exec_mode: "cluster", // Enable cluster mode for load balancing
+        instances: "max", // Adjust to the number of CPU cores or set a specific number
+        env: {
+          NODE_ENV: "production", // Environment variables
+          PORT: 3200, // Port to run the application
+        },
+      },
+    ],
+  };
   
